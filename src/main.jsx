6d2e4583d5624f82b059f9692872f37fdef406bd
Yeong <yjo0723@hotmail.com>
2023-02-09 20:04:26 -0800

    commit
    mobile header done
    sidebar done
    category done
    mostly structures and css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './normalize.css';
+import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import FeedbackDetail from './routes/FeedbackDetail';
+import NotFound from './routes/NotFound';
+import Home from './routes/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: 'feedback/:feedbackId',
+        element: <FeedbackDetail />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
