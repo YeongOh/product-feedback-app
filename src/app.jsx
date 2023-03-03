8679b761f66badc4e5daab22985c4e6de68980ed
Yeong <yjo0723@hotmail.com>
2023-03-02 16:30:52 -0800

    scrollToTop

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
+import ScrollToTop from './hooks/scrollToTop';
 
 export default function App() {
   return (
     <AuthContextProvider>
-      <Outlet />
+      <ScrollToTop>
+        <Outlet />
+      </ScrollToTop>
     </AuthContextProvider>
   );
 }
